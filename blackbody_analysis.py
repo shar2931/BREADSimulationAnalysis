076cfa6b6292c48ec559350055c4fe0858d843b5
Shardul Rao <shardulr2931@gmail.com>
2024-07-11 16:50:26 -0500

    Analysis and calculation code

--- blackbody_analysis.py
+++ blackbody_analysis.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description = 'Normalized Option')
+parser = argparse.ArgumentParser(description = 'FRED Data File Analyzer')
 parser.add_argument('--norm', type = bool, default = False)
 args = parser.parse_args()
-normalized = parser.norm
+normalized = args.norm
 
-numFiles = 20
-fpath = 'BlackbodyStudiesData/2024-07-02/'
+numFiles = 11
+fpath = 'BlackbodyStudiesData/2024-07-09/'
 fname = 'SNSPD-'
 
 plotDataXS = {}
@@ -55,49 +55,63 @@ zXS, fXS = zip(*sortedXS)
 nRaysXS = [irrad * 10**8 * (0.03333/21)**2 for irrad in fXS]
 errXS = np.sqrt(nRaysXS) * 10**-8 / ((0.03333 / 21) ** 2)
 
-errXS = np.sqrt(np.square(np.divide(errXS, fXS[10])), np.square(np.divide(np.multiply(errXS[10], errXS), fXS[10]**2)))
-fXS = np.multiply(fXS, 1 / fXS[10])
+if normalized:
+    errXS = np.sqrt(np.square(np.divide(errXS, fXS[10])), np.square(np.divide(np.multiply(errXS[10], errXS), fXS[10]**2)))
+    fXS = np.multiply(fXS, 1 / fXS[10])
+#print([(zXS[i], nRaysXS[i]) for i in range(len(fXS))])
 #plt.errorbar(zXS, fXS, yerr = errXS, fmt = 'o', label = '0.63mm x 0.63mm')
 
 planeS = list(plotDataS.keys())
 irradS = {pos[2]: plotDataS[pos] for pos in planeS}
-sortedS = sorted(irradS.items())    
+sortedS = sorted(irradS.items())[2:]    
 zS, fS = zip(*sortedS)
 
 nRaysS = [irrad * 10**8 * (0.03333/21)**2 for irrad in fS]
 errS = np.sqrt(nRaysS) * 10**-8 / ((0.03333 / 21) ** 2)
 
-errS = np.sqrt(np.square(np.divide(errS, fS[10])), np.square(np.divide(np.multiply(errS[10], errS), fS[10]**2)))
-fS = np.multiply(fS, 1 / fS[10])
+if normalized:
+    errS = np.sqrt(np.square(np.divide(errS, fS[8])), np.square(np.divide(np.multiply(errS[6], errS), fS[6]**2)))
+    fS = np.multiply(fS, 1 / fS[8])
 plt.errorbar(zS, fS, yerr = errS, fmt = 'o', label = '1.9mm x 1.9mm')
 
 planeM = list(plotDataM.keys())
 irradM = {pos[2]: plotDataM[pos] for pos in planeM}
-sortedM = sorted(irradM.items())    
+sortedM = sorted(irradM.items())[2:] 
 zM, fM = zip(*sortedM)
 
 nRaysM = [irrad * 10**8 * (0.03333/21)**2 for irrad in fM]
 errM = np.sqrt(nRaysM) * 10**-8 / ((0.03333 / 21) ** 2)
 
-errM = np.sqrt(np.square(np.divide(errM, fM[10])), np.square(np.divide(np.multiply(errM[10], errM), fM[10]**2)))
-fM = np.multiply(fM, 1 / fM[10])
+if normalized:
+    errM = np.sqrt(np.square(np.divide(errM, fM[8])), np.square(np.divide(np.multiply(errM[6], errM), fM[6]**2)))
+    fM = np.multiply(fM, 1 / fM[8])
 plt.errorbar(zM, fM, yerr = errM, fmt = 'o', label = '3.2mm x 3.2mm')
 
 planeL = list(plotDataL.keys())
 irradL = {pos[2]: plotDataL[pos] for pos in planeL}
-sortedL = sorted(irradL.items())    
+sortedL = sorted(irradL.items())[2:]    
 zL, fL = zip(*sortedL)
 
 nRaysL = [irrad * 10**8 * (0.03333/21)**2 for irrad in fL]
 errL = np.sqrt(nRaysL) * 10**-8 / ((0.03333 / 21) ** 2)
 
-errL = np.sqrt(np.square(np.divide(errL, fL[10])), np.square(np.divide(np.multiply(errL[10], errL), fL[10]**2)))
-fL = np.multiply(fL, 1 / fL[10])
+if normalized:
+    errL = np.sqrt(np.square(np.divide(errL, fL[8])), np.square(np.divide(np.multiply(errL[6], errL), fL[6]**2)))
+    fL = np.multiply(fL, 1 / fL[8])
 plt.errorbar(zL, fL, yerr = errL, fmt = 'o', label = '13.3mm x 13.3mm')
 
-plt.title('Normalized Power for Various Detector Sizes Along z-axis')
-plt.xlabel('z Shift from Focus (mm)')
-plt.ylabel('Normalized Power')
-plt.xticks(np.arange(-20, 20, step=4))
-plt.legend()
-plt.savefig('z-axis-irradiance-normalized.png')
+if normalized:
+    plt.title('Normalized Power for Various Detector Sizes Along z-axis')
+    plt.xlabel('z Shift from Focus (mm)')
+    plt.ylabel('Normalized Power')
+    plt.xticks(np.arange(-12, 10, step=4))
+    plt.legend()
+    plt.savefig('BlackbodyPlots/2024-07-09/z-axis-irradiance-normalized.png')
+
+else:
+    plt.title('Power for Various Detector Sizes Along z-axis')
+    plt.xlabel('z Shift from Focus (mm)')
+    plt.ylabel('Power')
+    plt.xticks(np.arange(-20, 20, step=4))
+    plt.legend()
+    plt.savefig('BlackbodyPlots/2024-07-09/z-axis-irradiance.png')
--- reflector_calculations.py
+++ reflector_calculations.py
@@ -1,17 +1,43 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 x1 = 15
 f = 201 * 2
-z = np.arange(-10.0, 10.0, 0.5)
+z = np.arange(-20.0, 20.5, 0.5)
 
 y1 = np.sqrt(4 * f * x1)
-phi1 = np.arctan((y1 ** 2) / ((y1 - z) * 4 * f))
+phi1 = np.arctan(x1 / (z + y1))
+phi2 = np.arctan((f / 2) / (z + 567.1))
 
-phi2 = np.arctan(f / (2 * (f * np.sqrt(2) - z)))
+zm = (((f / 2) - 15) * z - (f / 2) * 55.3) / 385
+#alpha = np.arctan(2 * (zm - z) / f)
+phiMax = np.arctan((f / 2) / (z - zm))
 
-y2 = ((f - 2 * x1) * z + f * y1) / (2 * f - 2 * x1)
-alpha = np.arctan(2 * (y2 - z) / f)
-phiMax = (np.pi / 2) - alpha
+#print(phi1 * 180 / np.pi)
+#print(phi2 * 180 / np.pi)
+#print(phiMax * 180 / np.pi)
+#print(np.abs(phi1 - phi2) / phiMax)
 
-print(phi1)
-print(np.abs(phi1 - phi2) / phiMax)
+c = 40
+R = 40
+r = 20
+
+#print(567.1 - 411.8)
+#print(z - ((c - z) * (x1 + r) / (R - r)))
+#print(z - ((c - z) * ((f/2) + r) / (R - r)))
+
+for z_i in z:
+    rPrime1 = np.arange(-1 * R, -1 * r, 0.01)
+    rPrime2 = np.arange(-1 * r + 0.01, 0.01, 0.01)
+
+    phiBottom1 = np.arctan((c - z_i) / (rPrime1 + r))
+    phiTop1 = np.arctan((c - z_i - 20) / (rPrime1 - r))
+
+    phiBottom2 = np.arctan((c - z_i - 20) / (rPrime2 + r))
+    phiTop2 = np.arctan((c - z_i - 20) / (rPrime2 - r))
+    print(phiBottom2 * 180 / np.pi)
+    print(phiTop2 * 180 / np.pi)
+
+    angles1 = np.abs(phiTop1 - phiBottom1) * 180 / np.pi
+    angles2 = np.abs(phiTop2 - phiBottom2) * 180 / np.pi
+    #print(np.sum(angles1) + np.sum(angles2))
